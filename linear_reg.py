e7bcd7d14c8ce33cfaef9032904abfb2ad9bfe6d
lorenzob <l.bolzani@gmail.com>
2019-06-23 23:25:03 +0200

    Update linear_reg.py

--- linear_reg.py
+++ linear_reg.py
@@ -1,5 +1,5 @@
+# Copyright 2019 Lorenzo Bolzani. All Rights Reserved.
 # Based on https://www.guru99.com/pytorch-tutorial.html
-# Author: Lorenzo Bolzani
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
