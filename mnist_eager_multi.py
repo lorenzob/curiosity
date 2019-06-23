e69d98d4ed01f6ba072450b028ebac5629a71e00
lorenzob <l.bolzani@gmail.com>
2019-06-23 23:24:31 +0200

    Update mnist_eager_multi.py

--- mnist_eager_multi.py
+++ mnist_eager_multi.py
@@ -1,4 +1,5 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2019 Lorenzo Bolzani. All Rights Reserved.
+# Based on TensorFlow Authors samples
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
