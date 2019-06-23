2cc1259e7e16cac9bc7a5bd636c58b2aa7aaaf91
lorenzob <l.bolzani@gmail.com>
2019-06-23 23:23:59 +0200

    Update mnist_curiosity.py

--- mnist_curiosity.py
+++ mnist_curiosity.py
@@ -1,4 +1,5 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Based on TensorFlow Authors samples
+# Author: Lorenzo Bolzani
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
