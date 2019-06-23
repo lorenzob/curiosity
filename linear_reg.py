1ec685be1ec959f2e738ebc16bc7f956ca9b6c18
lorenzob <l.bolzani@gmail.com>
2019-06-23 21:18:21 +0200

    Add files via upload

--- linear_reg.py
+++ linear_reg.py
@@ -34,15 +34,6 @@ print(net)
 # Visualize our data
 import matplotlib.pyplot as plt
 
-x = np.random.rand(100)
-y = np.sin(x) * np.power(x,3) + 3*x + np.random.rand(100)*0.8
-
-plt.scatter(x, y)
-#plt.show()
-
-# convert numpy array to tensor in shape of input size
-x = torch.from_numpy(x.reshape(-1,1)).float()
-y = torch.from_numpy(y.reshape(-1,1)).float()
 #print(x, y)
 
 # Define Optimizer and Loss Function
@@ -57,40 +48,58 @@ val_y = torch.from_numpy(val_y.reshape(-1,1)).float()
 val_inputs = Variable(val_x)
 val_outputs = Variable(val_y)
 
-inputs = Variable(x)
-outputs = Variable(y)
-for i in range(500):
-   prediction = net(inputs)
-   loss = loss_func(prediction, outputs) 
+for e in range(20):
 
-   # save individual losses for curiosity training
-   all_losses = F.mse_loss(prediction, outputs, reduction="none")
+  #print(e)
 
-   optimizer.zero_grad()
-   loss.backward()        
-   optimizer.step()       
+  x = np.random.rand(100)
+  y = np.sin(x) * np.power(x,3) + 3*x + np.random.rand(100)*0.8
 
-   # extra training on worst elements
-   curiosity = False
-   if curiosity:
+  # convert numpy array to tensor in shape of input size
+  x = torch.from_numpy(x.reshape(-1,1)).float()
+  y = torch.from_numpy(y.reshape(-1,1)).float()
 
-     # select worst items
-     k = 20
-     all_losses = all_losses.data.numpy().reshape(100,)
-     worst = np.argpartition(all_losses, -k)
-     worst_idx = worst[-k:]
+  inputs = Variable(x)
+  outputs = Variable(y)
+  for i in range(250):
 
-     r_inputs = inputs[worst_idx]
-     r_outputs = outputs[worst_idx]
+     BATCH_SIZE = len(inputs)
+
+     prediction = net(inputs)
+     loss = loss_func(prediction, outputs) 
+
+     # save individual losses for curiosity training
+     all_losses = F.mse_loss(prediction, outputs, reduction="none")
+
+     optimizer.zero_grad()
+     loss.backward()        
+     optimizer.step()
+
+     # extra training on worst elements
+     curiosity = True
+     curiosity_ratio = 0.2
+     k = int(round(BATCH_SIZE * curiosity_ratio))
+     if curiosity:
+       # select worst items
+       all_losses = all_losses.data.numpy().reshape(100,)
+       worst = np.argpartition(all_losses, -k)
+       retry_idx = worst[-k:]
+     else:
+       indexes = np.arange(BATCH_SIZE)
+       retry_idx = np.random.choice(indexes, size=k, replace=False)
+
+     r_inputs = inputs[retry_idx]
+     r_outputs = outputs[retry_idx]
 
      # train
      r_prediction = net(r_inputs)
      r_loss = loss_func(r_prediction, r_outputs)
      optimizer.zero_grad()
      r_loss.backward()        
-     optimizer.step()       
+     optimizer.step()
 
-   if i % 10 == 0:
+
+     if i % 10 == 0:
        # plot and show learning process
        val_prediction = net(val_inputs)
        val_loss = loss_func(val_prediction, val_outputs)
